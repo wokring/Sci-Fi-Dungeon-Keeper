99de3348b6f4a019d18aa47eeb2e7d3d2d892bbf
tohmais <callum.wishart@protonmail.com>
2022-08-27 09:30:35 +1000

    vectoring now works and is faster

--- src/main.js
+++ src/main.js
@@ -12,18 +12,16 @@ function animate() {
     requestAnimationFrame(animate);
     var obj = cube;
     var obj2 = cube2;
-    obj2.position.x = -2
-    obj2.position.y = 2
-    var angle = Math.atan((obj2.position.x - obj.position.x)/(obj2.position.y - obj.position.y));
-    if (Math.abs(obj2.position.x - obj.position.x) > 0.1) {
-        obj.position.x += 0.01 * Math.cos(angle);
-        obj.position.y += 0.01 * Math.sin(angle);
-    }
-    console.log(obj2.position.x - obj.position.x)
-    console.log(obj2.position.y - obj.position.y)
     
-    // obj.position.x += 0.01;
-    // obj.position.y += 0.01;
+    var x_diff = obj2.position.x - obj.position.x
+    var y_diff = obj2.position.y - obj.position.y
+    var nvector = ((x_diff) ** 2 + (y_diff) ** 2)**-0.5;
+    if ((Math.abs(x_diff) > 0.1) || (Math.abs(y_diff) > 0.1)){
+        obj.position.x += x_diff * nvector * 0.01;
+        obj.position.y += y_diff * nvector * 0.01;
+    }
+    obj2.position.y += 0.01;
+    console.log(obj2.position.y)
     renderer.render(scene, camera);
 }
 
@@ -31,10 +29,13 @@ function main() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
     scene.add(cube);
+    scene.add(cube2);
     camera.position.z = 5;
 
     const roomTree = new RoomTree(5, 5);
     if (WebGL.isWebGLAvailable()) {
+        cube2.position.x = -2
+        cube2.position.y = -2
         animate();
     } else {
         const warning = WebGL.getWebGLErrorMessage();
