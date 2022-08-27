cd4f40fb751869df4040206f74f3ec97bdce76a5
latewend <latewend@hotmail.com>
2022-08-27 11:40:48 +1000

    pan and placment

--- src/main.js
+++ src/main.js
@@ -1,35 +1,117 @@
 import { RoomTree, RoomNode } from "../modules/RoomTree.js" 
-import WebGL from "../js/WebGL.js";
+import WebGL from "../js/WebGL.js"; 
+const frustumSize = 10;
+let camera,aspect,scene,renderer,gui;
+let ghost;
+var test = false;
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+function init(){
+    scene = new THREE.Scene();
+    aspect = window.innerWidth / window.innerHeight;
+    camera = new THREE.OrthographicCamera(frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 1, 1000);
+    gui = [null,null,null,null,null];
+    gui[0] = camera;
+    camera.position.z = 5;
+    bar.position.y += -4;
+    bar.position.z += 3;
+    gui[1] = bar;
+    
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    scene.add(bar);
+
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+    document.addEventListener('keydown',onDocumentKeyDown, false);
+    document.addEventListener('mousedown', onDocumentMouseDown, false);
+    document.addEventListener('mouseup', onDocumentMouseUp, false);
+    
+}
+
+const plane1_1 = new THREE.PlaneGeometry( 1, 1 );
+
+function create_plane( x, y){
+    var plane =  new THREE.Mesh(plane1_1, new THREE.MeshBasicMaterial({ color: 0x00ff00 }));
+    plane.position.x = x;
+    plane.position.y = y;
+    return plane;
+}
+
+const geometry2 = new THREE.PlaneGeometry( 10, 1 );
+const material2 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const bar = new THREE.Mesh(geometry2,material2)
+
+var cube;
+
+var mx = 0;
+var my = 0;
+var tx = 0;
+var ty = 0;
+var mouse_down = false;
 
 function animate() {
     requestAnimationFrame(animate);
+    
+    if (!mouse_down){
+        var xd = tx -cube.position.x ;
+        var yd = ty -cube.position.y ;
+        var ins = Math.sqrt(xd**2 + yd**2);
+        if (Math.abs(ins) > 0.1){
+            cube.position.x += 0.05 * (xd/ins);
+            cube.position.y += 0.05 * (yd/ins);
+        }
+    }
+    if (test){
+        ghost.position.x = Math.ceil(mx - 0.5);
+        ghost.position.y = Math.ceil(my - 0.5);
+    }
     var obj = cube;
-    obj.rotation.x += 0.01;
-    obj.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 
-function main() {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-    scene.add(cube);
-    camera.position.z = 5;
+function onDocumentMouseDown( event ) {
+    mouse_down = true;    
+    tx = mx;
+    ty = my;
+    console.log(ty);
+    console.log(window.innerHeight);
+    console.log(window.innerWidth);
+    console.log(tx);
+}
+function onDocumentMouseUp( event ) {
+    mouse_down = false;
+}
+function onDocumentMouseMove(event) {
+    event.preventDefault();
+    if (mouse_down){
+        for (let i =0; i < 4; i++){
+            if (gui[i] != null){
+                gui[i].position.x -= event.movementX * 0.01;
+                gui[i].position.y += event.movementY * 0.01;
+            }
+        }        
+    }
+    mx = camera.position.x + aspect * frustumSize *((event.clientX/window.innerWidth)*2 -1) * 0.5;
+    my = camera.position.y + -frustumSize *((event.clientY/window.innerHeight)*2 -1) * 0.5;
+}
+function onDocumentKeyDown(event) {
+    if (event.key == 'a'){
+        test = true;
+        ghost = create_plane(0,0);
+        ghost.material.opacity = 0.1;
+        ghost.position.z = 3;
+        scene.add(ghost);
+    }
+}
 
-//     const grid = [
-//         [null, null],
-//         [null, null]
-//     ];
-//     const roomTree = new RoomTree(2, 2, grid);
-//     roomTree.makeRoot = 1;
-//     console.log(grid[0][1]);
-//     roomTree.root.addRoom(grid, 2, null);
+function main() {
+    init()
+    cube = create_plane(1,0);
+    scene.add(cube);
+    scene.add(create_plane(1,3));
+    scene.add(create_plane(2,0));
+
     if (WebGL.isWebGLAvailable()) {
         animate();
     } else {
@@ -37,5 +119,6 @@ function main() {
         document.getElementById("container").appendChild(warning);
     }
 }
+
 
 window.onload = main;
