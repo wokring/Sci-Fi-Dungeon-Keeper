56065c95f17aa42011c55f92e9e0931b9419773c
Miles Whiticker <miles.whiticker@gmail.com>
2022-08-27 18:52:46 +1000

    remove unused debug logging

--- modules/Spawner.js
+++ modules/Spawner.js
@@ -23,7 +23,7 @@ class Spawner{
         this._units = this._units.filter(unit => unit.health > 0);
         //If the spawnLimit has been reached, maintain the timer at maximum.
         //Or else we will continue the updates
-        console.log(this);
+        //console.log(this);
         if (this._units.length < this._spawnLimit) {
             //Update timer on unit kills
             //If the original unit count is equal to the spawn limit
@@ -33,7 +33,7 @@ class Spawner{
             }
             this._timeUntilNextSpawn--;
             if (this._timeUntilNextSpawn === 0) {
-                console.log(this);
+                //console.log(this);
                 this.spawn();
                 this._timeUntilNextSpawn = this._spawnSpan;
             }
@@ -44,9 +44,9 @@ class Spawner{
     spawn() {
         //Check it reach spawn limit or not
         if (this._spawnLimit - this._units.length > 0) {
-            console.log(this._template);
+            //console.log(this._template);
             let mob = new Unit(...this._template);
-            console.log(mob);
+            //console.log(mob);
             this._units.push(mob);
             return mob;
         } else {}
