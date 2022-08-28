6a29bc8c189921398d0c3eccc5ba18df28dca8c5
Miles Whiticker <miles.whiticker@gmail.com>
2022-08-28 11:03:33 +1000

    fix for traps not being rebuildable after they expire

--- modules/gui.js
+++ modules/gui.js
@@ -138,28 +138,38 @@ function onDocumentMouseDown( event ) {
             var x = mx-WORLD_MIN_X;
             var y = my-WORLD_MIN_Y;
             var buildSuccess = false;
-            switch(buildType){
-                case 1:
-                        buildSuccess = UIBuildRoom(buildType, new THREE.Vector2(x,y)); 
-                    break;
-                case 3:
-                    var room = DungeonRooms[x][y];
-                    if (room.isBuilt && room.trap == null){
-                        room.trap = new Spawner(room, [(scene, room)], 40, 4,x -3 ,y -3);
-                        scene.add(room.trap.sprite)
-                        buildSuccess = true;
-                    }
-                    break;
-                case 4:
-                    if (DungeonRooms[x][y].isBuilt && DungeonRooms[x][y].trap == null){
+		switch(buildType)
+		{
+			case 1:
+				buildSuccess = UIBuildRoom(buildType, new THREE.Vector2(x,y)); 
+				break;
+			case 3:
+				var room = DungeonRooms[x][y];
+				if (room.isBuilt && room.trap == null){
+				room.trap = new Spawner(room, [(scene, room)], 40, 4,x -3 ,y -3);
+				scene.add(room.trap.sprite)
+				buildSuccess = true;
+				}
+				break;
+			case 4:
+				var room = DungeonRooms[x][y];
+				if(!room.isBuilt)
+				{
+					console.log("Notice: Cannot build trap there, the room there is not built.");
+					break;
+				}
+				if (room.trap != null)
+				{
+					console.log("Notice: Cannot build trap there, already another trap present");
+					break;
+				}
+				
+				room.trap = new Trap(1,2,x -3 ,y -3, room);
+				scene.add(room.trap.sprite)
+				buildSuccess = true;
+				break;
 
-                        DungeonRooms[x][y].trap = new Trap(1,2,x -3 ,y -3);
-                        scene.add(DungeonRooms[x][y].trap.sprite)
-                        buildSuccess = true;
-                    }
-                    break;
-
-            }
+		}
             if(buildSuccess)
             {
                 power -= ROOM_COSTP[buildType];
